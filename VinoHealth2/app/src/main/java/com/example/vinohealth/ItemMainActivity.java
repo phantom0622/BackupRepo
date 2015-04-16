ed1d1f7248e2f9e71b84d724cc632e139a42e0c3
Peter Chien <peterkev5830@gamil.com>
2015-04-16 21:11:03 +0800

    Login style
    Login style and ProgressBar

---
+++ VinoHealth2/app/src/main/java/com/example/vinohealth/ItemMainActivity.java
@@ -0,0 +1,18 @@
+package com.example.vinohealth;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by peter on 2015/4/16.
+ */
+public class ItemMainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item);
+
+
+
+    }
+}
